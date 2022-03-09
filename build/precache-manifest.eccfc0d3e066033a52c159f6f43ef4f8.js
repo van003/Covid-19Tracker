be98b0b158b4236399894baeb04d68d783dfec61
van003 <67443894+van003@users.noreply.github.com>
2022-03-09 18:16:23 +0200

    update

---
+++ build/precache-manifest.eccfc0d3e066033a52c159f6f43ef4f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7bde32731e0b9fc43361010fc5be84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5625b8f189dfdb58da5a",
+    "url": "/static/css/main.e0fd1ce7.chunk.css"
+  },
+  {
+    "revision": "fab4c6bdad138cbfc83d",
+    "url": "/static/js/2.ab12b73d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ab12b73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5625b8f189dfdb58da5a",
+    "url": "/static/js/main.89b895bc.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "ca45ef5eada1e662a176d024dc0347c9",
+    "url": "/static/media/image.ca45ef5e.png"
+  }
+]);
